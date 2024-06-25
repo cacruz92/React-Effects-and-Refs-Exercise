14a468dcb7f4f80ff0079835fa059c0489d50dc4
Christian Cruz <christian.cruz92@hotmail.com>
2024-06-25 15:10:49 -0400

    added state to track the deck Id and the current card, added functions to choose a deck id and to pick a card

--- src/CardStack.js
+++ src/CardStack.js
@@ -1,8 +1,25 @@
 import React, {useState, useEffect} from "react";
+import axios from "axios";
 import Card from "./Card";
 import DrawButton from "./DrawButton";
 
 const CardStack = () => {
+    const[deckId, setDeckId] = useState(null);
+    const[currentCard, setCurrentCard] = useState(null);
+
+    useEffect(function getDeckId(){
+        async function fetchId(){
+            const res = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+            setDeckId(res.data.deck_id)
+        }
+        fetchId();
+    }, [])
+
+    const drawCard = async (deck_id) => {
+        const res = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`);
+        setCurrentCard(res.cards.code)
+        console.log(res.cards.code)
+    } 
 
     return (
         <>
