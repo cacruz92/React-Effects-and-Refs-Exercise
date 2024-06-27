dad1166b380be917ffac5c623ced200020b5a691
Christian Cruz <christian.cruz92@hotmail.com>
2024-06-26 23:01:58 -0400

    added functinoality to say there are no more cards left once all cards were drawn and added the file for the reshuffling of the existing deck

--- src/CardStack.js
+++ src/CardStack.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import axios from "axios";
 import Card from "./Card";
 import DrawButton from "./DrawButton";
+import ShuffleButton from "./ShuffleButton";
 
 const CardStack = () => {
     const[deckId, setDeckId] = useState(null);
@@ -17,19 +18,39 @@ const CardStack = () => {
     }, [])
 
     const drawCard = async (deck_id) => {
+        if(cardStack.length > 51){
+            alert("No more cards left!");
+            return;
+        }
         const res = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`);
         const newCard = res.data.cards[0]
         setCurrentCard(newCard);
-        setCardStack(cardStack => [...cardStack, newCard.image]);
-        console.log(cardStack);
+        setCardStack(cardStack => [...cardStack, newCard]);
     } 
+
+    const shuffleDeck = async(deck_id) => {
+        const rest = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/shuffle/`)
+    }
 
     return (
         <>
         <h1>Click to Draw!</h1>
-        <Card currentCard={currentCard}/>
-        {cardStack.length <52 && <DrawButton deckId={deckId} drawCard={drawCard}/>}
+        {cardStack.length < 52 ? (
+                <>
+                <DrawButton deckId={deckId} drawCard={drawCard} />
+                <Card currentCard={currentCard}/>
+                <ShuffleButton deckId={deckId}/>
+                </>
+            ) : (
+                <h1>No more cards left!</h1>
+            )}
+        <div>
+            {cardStack.map((card) => (
+                <img key={card.code} src={card.image} alt={`card-${card.code}`} />
+            ))}
+        </div>
         </>
+
     )
 }
 
